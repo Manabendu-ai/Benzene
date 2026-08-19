dc36b3c6f9a5a538d0b5bc55423f5ae80b8150ab
Manabendu-ai <rikugithub@gmail.com>
2026-08-19 12:04:59 +0530

    Handling the TOPIC CREATION with KafkaAdminClient

--- src/fastapi_producer/main.py
+++ src/fastapi_producer/main.py
@@ -8,3 +8,25 @@ from producer_schema import ProducerMessage
 KAFKA_BROKER = "localhost:9092"
 KAFKA_TOPIC = "benzene-topic"
 KAFKA_ADMIN_CLIENT = "benzene-admin-client"
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+
+    admin_client = KafkaAdminClient(
+        bootstrap_servers=KAFKA_BROKER,
+        client_id=KAFKA_ADMIN_CLIENT
+    )
+
+    if not KAFKA_TOPIC in admin_client.list_topics():
+        admin_client.create_topics(
+            new_topics=[
+                NewTopic(
+                    name=KAFKA_TOPIC,
+                    num_partitions=1,
+                    replication_factor=1
+                )
+            ],
+            validate_only=False
+        )
+
+    yield
