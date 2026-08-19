b5519838fde03f4b90d539f5ea7173bb2d7bb1b8
Manabendu-ai <rikugithub@gmail.com>
2026-08-19 12:35:58 +0530

    created a polling event

--- src/fastapi_consumer/main.py
+++ src/fastapi_consumer/main.py
@@ -6,3 +6,10 @@ import json
 KAFKA_BROKER="localhost:9092"
 KAFKA_topic="benzene-topic"
 KAFKA_CONSUMER_ID="benzene_consumer"
+
+stop_polling_event = asyncio.Event()
+app = FastAPI(
+    title="Benzene-Consumer",
+    description="An Event Driven Architechture",
+    version="1.0.0",
+)
