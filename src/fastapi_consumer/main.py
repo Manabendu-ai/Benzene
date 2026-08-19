42b37da2f0c97a1d7f665d0154662b45898af4f3
Manabendu-ai <rikugithub@gmail.com>
2026-08-19 12:33:04 +0530

    defined constants

--- src/fastapi_consumer/main.py
+++ src/fastapi_consumer/main.py
@@ -2,3 +2,7 @@ from fastapi import FastAPI
 import asyncio
 from kafka import KafkaConsumer
 import json
+
+KAFKA_BROKER="localhost:9092"
+KAFKA_topic="benzene-topic"
+KAFKA_CONSUMER_ID="benzene_consumer"
