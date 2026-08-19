cb43b2a21671800e99221cb9954c76abab075b08
Manabendu-ai <rikugithub@gmail.com>
2026-08-19 11:47:03 +0530

    constant value section

--- src/fastapi_producer/kafka_producer.py
+++ src/fastapi_producer/kafka_producer.py
@@ -2,3 +2,7 @@ from kafka import KafkaProducer
 from fastapi import HTTPException
 from producer_schema import ProducerMessage
 import json
+
+KAFKA_BROKER = "localhost:9092"
+KAFKA_TOPIC = "benzene-topic"
+PRODUCER_CLIENT_ID = "benzene_producer"
