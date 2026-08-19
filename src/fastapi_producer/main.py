4c6716bb8e7a9496cca7984cdd7c751c6d4956e4
Manabendu-ai <rikugithub@gmail.com>
2026-08-19 11:58:10 +0530

    added the constants

--- src/fastapi_producer/main.py
+++ src/fastapi_producer/main.py
@@ -3,3 +3,8 @@ from kafka.admin import KafkaAdminClient, NewTopic
 from kafka_producer import produce_kafka_message
 from contextlib import asynccontextmanager
 from producer_schema import ProducerMessage
+
+
+KAFKA_BROKER = "localhost:9092"
+KAFKA_TOPIC = "benzene-topic"
+KAFKA_ADMIN_CLIENT = "benzene-admin-client"
